eed96139503d6e4c3dc0521af2bd4cf76dc316b9
Princy <57955061+princys-lab@users.noreply.github.com>
2021-03-04 10:14:20 +0100

    (feature) :added face detection

--- drone.py
+++ drone.py
@@ -22,3 +22,26 @@ class MyDrone(Tello):
         frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)  # Resizing both axes by 1/4
 
         return frame
+
+    @staticmethod
+    def detect_face(img):
+        face = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face.detectMultiScale(img_grey, 1.2, 4)
+        face_list = []
+        face_area = []
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cx = x + w // 2
+            cy = y + h // 2
+            area = w * h
+            face_list.append([cx, cy])
+            face_area.append(area)
+        if len(face_area) != 0:
+            i = face_area.index(max(face_area))
+
+            return img, [face_list[i], face_area[i]]
+
+        else:
+            return img, [[0, 0], 0]
--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
 
     while True:
         img = drone.get_fame()
+        img, info = drone.detect_face(img)
+        print(info)
         cv2.imshow('Drone Output', img)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
